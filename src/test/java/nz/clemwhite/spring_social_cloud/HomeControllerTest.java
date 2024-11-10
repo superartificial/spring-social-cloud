e2f4235f410151c0b821738faf627258d7bc5a0a
Clem White <clemw@hotmail.com>
2024-11-10 13:22:16 +1300

    Added HomeControllerTest

---
+++ src/test/java/nz/clemwhite/spring_social_cloud/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package nz.clemwhite.spring_social_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldDisplayHomePage() throws Exception {
+        // before static imports
+//        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+}
